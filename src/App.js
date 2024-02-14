a6f5a41047ef77831b10b27479ab559b6c2681a2
nayanatara07 <nayana2017tara@gmail.com>
2024-02-14 08:52:52 +0530

    adding logo with website link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BoxComponent from './BoxComponent';
+import logo from './logo.png'; 
+import './App.css'; 
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<BoxComponent />} />
+      <div>
         {}
-      </Routes>
+        <a href="https://www.faceprep.in/" target="_blank" rel="noopener noreferrer">
+          <img src={logo} alt="Logo" className="logo" />
+        </a>
+        <Routes>
+          <Route path="/" element={<BoxComponent />} />
+          {}
+        </Routes>
+      </div>
     </Router>
   );
 }
